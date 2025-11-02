e43ce7a2263303e417167c786af689763025c79c
José Ángel Huerta Amparán <josehuerta357@outlook.com>
2025-11-01 18:13:47 -0700

    Desv. est. para qc

--- fog-layer/data_collector.py
+++ fog-layer/data_collector.py
@@ -4,6 +4,8 @@ import mysql.connector
 from datetime import datetime
 import os
 from dotenv import load_dotenv
+from qc import SimpleQualityControl
+
 
 import paho.mqtt.client as paho  # Cliente para la comunicación local (ESP32)
 from azure.iot.device import IoTHubDeviceClient, Message # Cliente para la comunicación a la Nube (Azure)
@@ -24,23 +26,44 @@ LOCAL_MQTT_TOPIC = os.getenv('MQTT_TOPIC', 'sensor/parking/esp')
 azure_client = None
 local_mqtt_client = None
 
+# Variable de control de calidad
+qc_estadistico = SimpleQualityControl(window_size=10, z_threshold=2.5)
+
 # Aseguramiento de calidad de datos en la niebla
 def aplicar_qc(datos):
     temp = datos.get('temp_celsius', None)
+    humedad = datos.get('humedad_porcentaje', None)
+    luz = datos.get('luz_adc', None)
+    distancia = datos.get('distancia_cm', None)
+
     TEMP_MIN_LOGICO = -10.0
     TEMP_MAX_LOGICO = 60.0
 
-    # Validacion en el rango de temperatura logico
+    # Faltaría agregar valores lógicos para los otros tipos de datos :)
+
+    # Validación en el rango lógico
     if temp is None or temp < TEMP_MIN_LOGICO or temp > TEMP_MAX_LOGICO:
-        print(f"Lectura de temperatura anomala detectada: {temp} C°.")
+        print(f"Lectura de temperatura anómala detectada: {temp} °C")
         return False
 
-    # Validacion de humedad y luz no nulas
-    if datos.get('humedad_porcentaje', None) is None or datos.getr('luz_adc', None) is None:
+    # Validación de humedad y luz no nulas
+    if humedad is None or luz is None:
         print("Lectura de datos incompleta. Revise los sensores.")
-        return False 
+        return False
 
-    return True
+    # Si pasa las validaciones lógicas, aplicar control de calidad estadístico
+    resultado_qc = qc.aplicar_qc({
+        'temperatura': temp,
+        'humedad': humedad,
+        'luz_adc': luz,
+        'distancia_cm': distancia if distancia is not None else 0
+    })
+
+    if not resultado_qc['todos_aprobados']:
+        print("Advertencia: valores fuera de rango estadístico")
+        print(resultado_qc['resultados'])
+
+    return resultado_qc['todos_aprobados']
 
 
 # Crear la conexion con el servicio de azure
